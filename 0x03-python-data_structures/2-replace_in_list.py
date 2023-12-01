43259d47f0445bb254f77340cc623e54eeb0a8dc
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-01 00:04:27 -0800

    goo

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if(idx < ) 0 or idx >= len(mylist)):
+    if(idx < 0 or idx >= len(mylist)):
         return(my_list)
     my_list[idx] = element
-    return(mu_list)
+    return(my_list)

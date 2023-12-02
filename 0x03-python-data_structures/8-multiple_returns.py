b94917350df0f818d4cf23941df701eb1383e090
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-01 23:04:50 -0800

    gooos

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if(len(sentence) > 0):
-        return((len(sentence), sentence[0]))
-    return((len(sentence), None))
+    """
+    Returns a tuple with the length of a string and its first character
+    ...
+
+    Parameters
+    ----------
+    sentence : str
+        The string to treat
+
+    Return:
+        A tuple
+    """
+
+    if len(sentence) == 0:
+        return (0, None)
+    else:
+        return len(sentence), sentence[0]

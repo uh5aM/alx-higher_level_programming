50f8d7cd8302b128e33baeaf23a73ae5a2d5d406
uh5aM <ahmadabubakarmusa0@gmail.com>
2023-12-01 22:55:52 -0800

    giid

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for y in my_list:
-        print("{}".format(y))
+    """
+    Print a list of integers
+    ...
+
+    Parameters
+    ----------
+    my_list : list optional
+        The list of integers
+
+    Return:
+        None
+    """
+
+    for element in my_list:
+        print("{:d}".format(element))

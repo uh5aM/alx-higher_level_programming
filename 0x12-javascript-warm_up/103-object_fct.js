b7fe149eb62cf80e82ed960791577dcc446cc65c
uh5aM <ahmadabubakarmusa0@gmail.com>
2024-03-11 21:23:18 +0000

    good

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+i#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
